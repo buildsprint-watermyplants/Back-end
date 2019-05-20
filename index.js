ffc900908b039034a2e46a3ac336f27e9646ab14
Mckay <mckaywrigley@gmail.com>
2019-05-19 20:16:47 -0700

    creates generate token helper

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/auth");
