0f4d2331124d00dc9f18080801d56a5e47cfc0f3
Mckay <mckaywrigley@gmail.com>
2019-05-19 20:06:39 -0700

    creates user routes

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 
 app.use(express.json());
 
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+
+app.use("/auth/", authRoutes);
+app.use("/api/", userRoutes);
+
 const port = 3000;
 
 app.listen(port, () => {
---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+const Users = require("../users/users-model.js");
+
+router.get("/", (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+router.get("/:id", (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => res.send(err));
+});
