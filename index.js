8f9e5eaa175f45a2d7b0bb4e9400770873e83361
Mckay <mckaywrigley@gmail.com>
2019-05-20 10:38:11 -0700

    adds env

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/plants", plantRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}.`);
