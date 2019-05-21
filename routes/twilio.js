3221d3c23642697d1765d160d5f91bfe950b654c
Mckay <mckaywrigley@gmail.com>
2019-05-21 13:24:02 -0700

    simplifies twilio routes

--- routes/twilio.js
+++ routes/twilio.js
@@ -8,28 +8,17 @@ const getTimeZones = function() {
   return momentTimeZone.tz.names();
 };
 
-// GET: /plants
 router.get("/", function(req, res, next) {
   Reminder.find().then(reminders => {
     res.status(200).json(reminders);
   });
 });
 
-// GET: /plants/create
-router.get("/create", function(req, res, next) {
-  res.send({
-    timeZones: getTimeZones(),
-    reminder: new Reminder({
-      plantName: "",
-      phoneNumber: "",
-      notification: "",
-      timeZone: "",
-      time: ""
-    })
-  });
+router.get("/:id", function(req, res, next) {
+  const id = req.params.id;
+  Plant.findOne({ _id: id }).then(function(plant) {});
 });
 
-// POST: /plants
 router.post("/", function(req, res, next) {
   const plantName = req.body.plantName;
   const phoneNumber = req.body.phoneNumber;
@@ -50,14 +39,7 @@ router.post("/", function(req, res, next) {
     .catch(err => res.json(err));
 });
 
-// GET: /plants/:id/edit
-router.get("/:id/edit", function(req, res, next) {
-  const id = req.params.id;
-  Plant.findOne({ _id: id }).then(function(plant) {});
-});
-
-// POST: /plants/:id/edit
-router.post("/:id/edit", function(req, res, next) {
+router.put("/:id", function(req, res, next) {
   const id = req.params.id;
   const plantName = req.body.plantName;
   const phoneNumber = req.body.phoneNumber;
@@ -65,14 +47,14 @@ router.post("/:id/edit", function(req, res, next) {
   const timeZone = req.body.timeZone;
   const time = moment(req.body.time, "MM-DD-YYYY hh:mma");
 
-  Reminders.findOne({ _id: id }).then(function(plant) {
+  Reminder.findOne({ _id: id }).then(function(plant) {
     plant.plantName = plantName;
     plant.phoneNumber = phoneNumber;
     plant.notification = notification;
     plant.timeZone = timeZone;
     plant.time = time;
 
-    Reminders.save().then(function() {});
+    Reminder.save().then(function() {});
   });
 });
 
@@ -80,64 +62,7 @@ router.post("/:id/edit", function(req, res, next) {
 router.post("/:id/delete", function(req, res, next) {
   const id = req.params.id;
 
-  Reminders.remove({ _id: id }).then(function() {});
+  Reminder.remove({ _id: id }).then(function() {});
 });
 
 module.exports = router;
-
-// const express = require("express");
-// const twilio = require("twilio");
-// const keys = require("../config/keys");
-// const client = require("twilio")(keys.twilioAccountSid, keys.twilioAuthToken);
-// const CronJob = require("cron").CronJob;
-// const notificationsWorker = require("./workers/notificationsWorker");
-// const moment = require("moment");
-
-// const router = express.Router();
-
-// const restricted = require("../helpers/auth/restricted");
-
-// const Plants = require("../db/models/Plants");
-
-// router.post("/create", (req, res) => {
-//   const { phoneNumber } = req.body;
-//   client.messages
-//     .create({
-//       from: keys.twilioNumber,
-//       body: "It's time to water your plants!",
-//       to: phoneNumber
-//     })
-//     .then(message => {
-//       return res.status(200).json(message);
-//     })
-//     .catch(err => res.status(500).json(err));
-// });
-
-// const notificationWorkerFactory = function() {
-//   return {
-//     run: function() {
-//       Plants.sendNotifications();
-//     }
-//   };
-// };
-
-// const schedulerFactory = function() {
-//   return {
-//     start: function() {
-//       new CronJob(
-//         "00 * * * * *",
-//         function() {
-//           console.log(
-//             "Running Send Notifications Worker for " + moment().format()
-//           );
-//           notificationsWorker.run();
-//         },
-//         null,
-//         true,
-//         ""
-//       );
-//     }
-//   };
-// };
-
-// module.exports = router;
