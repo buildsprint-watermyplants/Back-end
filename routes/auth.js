99cd26dc3090721253e8c41fbee14f03cf7b3f3c
Mckay <mckaywrigley@gmail.com>
2019-05-21 08:08:28 -0700

    returns user object with login

--- routes/auth.js
+++ routes/auth.js
@@ -40,7 +40,7 @@ router.post("/login", (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = createToken.generateToken(user);
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          user,
           token
         });
       } else {
