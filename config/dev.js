c2683ccd4fe4276636a500de5f4681c77d5d5d26
cecil john tantay <cjbtantay@gmail.com>
2019-08-21 16:45:17 -0700

    add dev.js

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET,
+  twilioAccountSid: process.env.TWILIO_ACCOUNT_SID,
+  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
+  twilioNumber: process.env.TWILIO_NUMBER,
+  mongoURI: process.env.MONGO_URI
+};
